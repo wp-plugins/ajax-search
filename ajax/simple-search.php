25434cfc7f582a97ad6d98dbba6dc946543a1c7d
almos20 <almos20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-14 11:03:29 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/ajax-search/tags/1.0.2@557887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax/simple-search.php
+++ ajax/simple-search.php
@@ -1,30 +1,30 @@
 <?php
 
-include_once '../../../../wp-load.php';
+	include_once '../../../../wp-load.php';
 
-$str = addslashes($_POST['s']);
+	function filter_where( $where = '')
+	{
+    	$str = addslashes($_GET['s']);
 
-function filter_where( $where = '')
-{
-  global $str;
-  
-  $where .= " AND post_title LIKE '%$str%' ";
-  
-  return $where;
-}
+	    $where .= " AND post_title LIKE '%$str%' ";
+	  
+	    return $where;
+	}
 
- 
-add_filter('posts_where', 'filter_where');
+	add_filter('posts_where', 'filter_where');
 
-$ajax_query = new WP_Query('post_status=published');
+	$ajax_query = new WP_Query('post_status=published');
 
-remove_filter('posts_where', 'filter_where');
+	remove_filter('posts_where', 'filter_where');
 
-while ($ajax_query->have_posts()) : $ajax_query->the_post();
-?>
-  <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
-<?php
-  echo '<br/>';
- 
-endwhile;
-
+	/* The LOOP */
+	echo '<ul>';
+	while ($ajax_query->have_posts()) : $ajax_query->the_post();
+		?>
+        <li>
+		  <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
+		</li>
+		<?php
+		 
+	endwhile;
+    echo '</ul>';
