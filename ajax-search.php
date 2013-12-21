98ac86b90fa19e2ef036e6083d5620f19072b62a
almos20 <almos20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-21 07:51:33 +0000

    tag version 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/ajax-search/tags/1.1.0@826491 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax-search.php
+++ ajax-search.php
@@ -4,7 +4,7 @@ Plugin Name: Ajax search
 Plugin URI: http://sandorkovacs84.wordpress.com/
 Description: Search your posts and pages instant. 
 Author: Sandor Kovacs
-Version: 1.0
+Version: 1.1.0
 Author URI: http://sandorkovacs84.wordpress.com/
 */
  
@@ -100,7 +100,7 @@ class MXAjaxSearchWidget extends WP_Widget
     ?>
     <!-- Instant search form -->
     <div id='mx-ajax-search'>
-      <input type='text' name='my-s' id='my-s' />
+      <input type='text' name='my-s' id='my-s' style="width:100%;"/>
     </div>
     
     <!-- Display search results -->
@@ -111,7 +111,7 @@ class MXAjaxSearchWidget extends WP_Widget
       jQuery('#my-s').keyup(function() {
         jQuery.ajax({
           'type':   'get',
-          'url' :   '<?php echo $plugin_url.'/ajax/simple-search.php'; ?>',
+          'url' :   '<?php echo $plugin_url.'ajax/simple-search.php'; ?>',
           'data':   's=' + jQuery('#my-s').val(),
           'success': function (result) {
             jQuery('#results').html(result);
--- ajax/simple-search.php
+++ ajax/simple-search.php
@@ -1,30 +1,28 @@
 <?php
+include_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/wp-load.php';
 
-	include_once '../../../../wp-load.php';
+$str = addslashes($_GET['s']);
+function filter_where($where = '') {
+    $str = addslashes($_GET['s']);
 
-	function filter_where( $where = '')
-	{
-    	$str = addslashes($_GET['s']);
+    $where .= " AND post_title LIKE '%$str%' ";
 
-	    $where .= " AND post_title LIKE '%$str%' ";
-	  
-	    return $where;
-	}
+    return $where;
+}
 
-	add_filter('posts_where', 'filter_where');
+add_filter('posts_where', 'filter_where');
 
-	$ajax_query = new WP_Query('post_status=published');
+$ajax_query = new WP_Query('post_status=publish');
 
-	remove_filter('posts_where', 'filter_where');
+remove_filter('posts_where', 'filter_where');
 
-	/* The LOOP */
-	echo '<ul>';
-	while ($ajax_query->have_posts()) : $ajax_query->the_post();
-		?>
-        <li>
-		  <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
-		</li>
-		<?php
-		 
-	endwhile;
-    echo '</ul>';
+/* The LOOP */
+echo '<ul id="simple-ajax-search-result-list">';
+while ($ajax_query->have_posts()) : $ajax_query->the_post();
+    ?>
+    <li>
+        <a href='<?php the_permalink(); ?>'><?php echo strtoupper(str_replace($str, '<strong>' . $str . '</strong>', strtolower(get_the_title()))); ?></a>
+    </li>
+    <?php
+endwhile;
+echo '</ul>';
